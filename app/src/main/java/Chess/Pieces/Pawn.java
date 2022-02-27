a720c5c09de4cd46f56e06f6616478867289bc1a
TheColdZ <thomas-vinther@hotmail.com>
2022-02-27 16:44:18 +0100

    pawn move 2 as first move

--- app/src/main/java/Chess/Pieces/Pawn.java
+++ app/src/main/java/Chess/Pieces/Pawn.java
@@ -5,11 +5,13 @@ import Chess.Piece;
 public class Pawn implements Piece {
     private final String name;
     private final boolean colour; //true = white, false = black
+    private boolean hasMoved;
     
 
     public Pawn(boolean colour){
         this.name = "Pawn";
-        this.colour = colour; 
+        this.colour = colour;
+        this.hasMoved = false; 
     }
 
     public String getName() {
@@ -17,6 +19,14 @@ public class Pawn implements Piece {
     }
 
     public boolean canMoveToSquare(int fromRow,int fromColumn, int toRow, int toColumn){
+        if(!this.hasMoved){
+            this.hasMoved = true;
+            if (this.colour) {
+                return (fromColumn == toColumn) && (fromRow - 2 <= toRow);
+            }
+            return (fromColumn == toColumn) && (fromRow + 2 >= toRow);
+        }
+
         if (this.colour) {
             return (fromColumn == toColumn) && (fromRow - 1 == toRow);
         }
--- app/src/test/java/Chess/BoardTest.java
+++ app/src/test/java/Chess/BoardTest.java
@@ -50,6 +50,19 @@ class BoardTest {
         assertNotNull(board.getPieceAt(1, 0));
     }
 
+    @Test void firstPawnMoveCanBe2(){
+        Board board = new Board();
+        board.movePiece(6, 0, 4, 0);
+        assertNotNull(board.getPieceAt(4, 0));
+
+    }
+
+    @Test void secondMoveOfPawnCannotBe2(){
+        Board board = new Board();
+        board.movePiece(6, 0, 5, 0);
+        board.movePiece(5, 0, 3, 0);
+        assertNull(board.getPieceAt(3, 0));
+    }
 
     
 
